519e096fa65c1531404fd66d206deafafe6ff234
gogogojira <frankadelic4u@gmail.com>
2019-11-12 21:59:46 +0000

    invalid entry

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  
+
   def index
     @places = Place.all
   end
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -34,6 +38,11 @@ class PlacesController < ApplicationController
      end
 
      @place.update_attributes(place_params)
+     if @place.valid?
+       redirect_to root_path
+     else
+       render :edit, status: :unprocessable_entity
+     end
   end
 
   def destroy
